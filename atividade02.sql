f23422b83d5f6ddbf4ba83023eb2836b5fbd70d3
Ollyveroso <126161749+Ollyveroso@users.noreply.github.com>
2024-03-28 08:34:32 -0300

    Update atividade02.sql

--- atividade02.sql
+++ atividade02.sql
@@ -5,3 +5,7 @@ SELECT * FROM alunos WHERE idade = '16';
 --esse codigo procura só quem tem o sobrenome Santos, mas se não filtrar utilize o sobrenome 'dos Santos' que o codigo funcionará.--
 
 SELECT * FROM alunos WHERE sobrenome = 'dos Santos' 
+
+--mas eu acabei de aprender 8:33 da manhã um codigo para colocar só o sobrenome Santos.
+
+SELECT * FROM alunos WHERE sobrenome LIKE '%Santos%'
