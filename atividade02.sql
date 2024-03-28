7ab2b8590b2eade1ffd531348f06a12f7fad038e
Ollyveroso <126161749+Ollyveroso@users.noreply.github.com>
2024-03-28 08:17:49 -0300

    Create atividade02.sql

---
+++ atividade02.sql
@@ -0,0 +1,7 @@
+--esse codigo procura só quem tem a idade igual 16.
+
+SELECT * FROM alunos WHERE idade = '16';
+
+--esse codigo procura só quem tem o sobrenome Santos, mas se não filtrar utilize o sobrenome 'dos Santos' que o codigo funcionará.--
+
+SELECT * FROM alunos WHERE sobrenome = 'dos Santos' 
